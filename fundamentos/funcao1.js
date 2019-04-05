b2399b8663cd0d7491b1c54c3ec4da3fb9b1875a
thejoaov <jvictorsantos852@gmail.com>
2019-04-05 13:30:29 -0300

    q

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,14 @@
+// Funcao sem retorno
+function imprimirSoma(a, b){
+    console.log(a+b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+
+// Funcao com retorno
+function soma(a, b = 0){
+    return a + b
+}
+
+console.log(soma(2))

25f07aab70814194381e0cb0e09026f639260425
thejaov <jvictorsantos852@gmail.com>
2019-04-07 18:25:15 -0300

    ggc

---
+++ funcoes/iife.js
@@ -0,0 +1,7 @@
+// IIFE -> Immediatly Invoked Function Expression
+// Funções Auto-Invocadas
+
+(function(){
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()

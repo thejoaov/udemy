7c0ebddc22de18be72139e3a5aa5eacb8b9013ac
thejoaov <jvictorsantos852@gmail.com>
2019-04-10 20:13:16 -0300

    qeqwe

---
+++ array/map2.js
@@ -0,0 +1,10 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+// Retornar um array apenas com os preços
+let precos = carrinho.map(e => JSON.parse(e).preco)
+console.log(precos)

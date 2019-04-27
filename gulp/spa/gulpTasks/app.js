0b9524536d7f6b6dca3a1257bbbbe4a551d38637
thejoaov <jvictorsantos852@gmail.com>
2019-04-27 04:41:03 -0300

    khjv

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -15,13 +15,23 @@ gulp.task('app.html', () =>{
 })
 
 gulp.task('app.css', () =>{
-    
+    return gulp.src('src/assets/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({"uglifyComments":true}))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 })
 
 gulp.task('app.js', () =>{
-    
+    return gulp.src('src/assets/js/**/*.js')
+        .pipe(babel({presets: ['env']}))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/assets/js'))
+
 })
 
 gulp.task('app.imgs', () =>{
-    
+    return gulp.src('src/assets/imgs/**/*.*')
+        .pipe(gulp.dest('build/assets/imgs'))
 })
